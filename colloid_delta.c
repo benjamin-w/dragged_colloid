a68ce20bb8c65d65092ce2f305b5e3418a51d148
sonarventu <dventure@sissa.it>
2021-06-01 18:59:37 +0200

    added interaction with colloid

--- colloid_delta.c
+++ colloid_delta.c
@@ -1,18 +1,12 @@
 // 02.05.2021 - Colloid in a fluctuating scalar field
 // Davide Venturelli & Benjamin Walter
-// Last update 27.05.2021
+// Last update 01.06.2021
 // Compile as $ gcc -o colloid colloid.c -lm -lgsl
 /* COMMENTS:
 - Stochastic Runge-Kutta II for colloid evolution, Euler-Maruyama for field evolution (can be enhanced, but the price is O(N) at least). We could even think of anisotropic resolution (better around the colloid).
 - No boundary conditions on the colloid displacement; they only get enforced when locating the nearest site.
 - Space is measured in units of the lattice spacing.
-TODO
-- Chiedere a Urna quanto grande serve prendere il reticolo
-- Nella routine di Ben per la lista di primi vicini (che comunque non sto più usando),
-	(*neighbours)[pos][0] = (((ix + 1) % L) + yshift + zshift);
-	(*neighbours)[pos][1] = (((ix - 1) % L) + yshift + zshift);
-mentre a me avevano insegnato a fare ((ix + L - 1) % L). Capire che differenza c'è e se Ben usava questa cosa consapevolmente.
-- Test max_crown for small R (but it's a mild improvement)
+- Point-like colloid version.
 */
 
 // LIBRARIES, TYPES, DEFINITIONS
@@ -39,7 +33,6 @@ typedef struct{
 	double temperature;													// Temperature of the common bath
 	double relativeD;													// Ratio of colloid to field mobility
 	double trap_strength;												// Stiffness of the harmonic trap
-	int R;																// Size of colloid in lattice units
 	int rng_seed;														// Seed for random number generator
 	int system_size;													// Side of the DIM-dimensional lattice
 	double delta_t;														// Time-discretization step
@@ -53,15 +46,15 @@ gsl_rng *r;
 // FUNCTION PROTOTYPES
 
 void default_parameters(parameter*);
-int initialise(double**, double**, long***, long, parameter*,long***);
-void evolveB(double**, double**, long**, long, long, parameter*,long**,int,long**,int);
-void evolveA(double**, double**, long**, long, long, parameter*,long**,int);
+void initialise(double**, double**, long***, long, parameter*);
+void evolveB(double**, double**, long**, long, long, parameter*);
+void evolveA(double**, double**, long**, long, long, parameter*);
 void laplacian(double**, double*, long**, long);
 void laplacian_of_cube(double**, double*, long**, long);
 void generate_noise_field(double**, long, parameter*);
 void gradient_field(double**, double*, long**, long);
-void phi_evolveB(double**, double*, double*, double*, double*, long, parameter*, double*, long**, int);
-void phi_evolveA(double**, double*, double*, long, parameter*, double*, int, long**);
+void phi_evolveB(double**, double*, double*, double*, double*, long, parameter*, double*, long **);
+void phi_evolveA(double**, double*, double*, long, parameter*, double*);
 void measure(double**, long, parameter*);
 void printhelp(void);
 void print_source(void);
@@ -86,7 +79,7 @@ int main(int argc, char *argv[]){
 	opterr = 0;
 	int c = 0;
 	// ./colloid -r 2 -L 23 (order doesn't count)
-    while( (c = getopt (argc, argv, "L:r:l:u:T:D:k:S:N:R:hX") ) != -1){
+    while( (c = getopt (argc, argv, "L:r:l:u:T:D:k:S:N:hX") ) != -1){
     	switch(c){
 			case 'L':
 				params.system_size = atoi(optarg);
@@ -112,9 +105,6 @@ int main(int argc, char *argv[]){
 			case 'S':
 				params.rng_seed = atoi(optarg);
 				break;
-			case 'R':
-				params.R = atoi(optarg);
-				break;
 			case 'X':
 				print_source();
 				exit(2);
@@ -135,67 +125,28 @@ int main(int argc, char *argv[]){
 	double* phi; 														// Field on lattice NxNxN
 	double* y_colloid; 													// Colloid position (DIM real numbers)
 	long** neighbours; 													// i x j - table with j neighbours of site i
-	long** mosaic;														// table with neighbours of site i with distance <R
 	
 	ALLOC(phi, n_sites);
 	CALLOC(y_colloid, DIM);
 	ALLOC(neighbours, n_sites);
-	ALLOC(mosaic, n_sites);
 
 	for(i = 0; i < n_sites; i++) CALLOC(neighbours[i], 2 * DIM); 		// At each index there are 2*D neighbours
-	
-	int max_within_R = (int)(factorial(DIM)/factorial(2*DIM)/M_PI * floatpow(sqrt(M_PI)*4*params.R,DIM));
-	int max_crown = max_within_R - (int)(factorial(DIM)/factorial(2*DIM)/M_PI * floatpow(sqrt(M_PI)*4*(params.R-1),DIM));
-	int top=0;															// Actual sites within R around a single site
-	for(i = 0; i < n_sites; i++) CALLOC(mosaic[i], max_within_R); 		// At each index, all the sites within R
-		
 
 	// Numerical integration of the dynamics
 	
-	top = initialise(&phi, &y_colloid, &neighbours, n_sites, &params, &mosaic);
+	initialise(&phi, &y_colloid, &neighbours, n_sites, &params);
 	
 	if(MOD==0){															// Model A
-		evolveA(&phi, &y_colloid, neighbours, n_sites, n_timestep, &params, mosaic, top);
+		evolveA(&phi, &y_colloid, neighbours, n_sites, n_timestep, &params);
 	}
 	else{																// Model B
-		long** crown;													// Table with neighbours of site i with distance R-a<r<R
-		ALLOC(crown, n_sites);
-		for(i = 0; i < n_sites; i++) CALLOC(crown[i], max_within_R); 	// At each index, all the sites with distance R-a<r<R. I could use max_crown but it's untested.
-		
-		// Initialize crown (should be put in an external function if it works)
-		double dist;
-		int c_top, d, j;
-		long L = params.system_size;
-		for(i=0; i<n_sites; i++){
-			c_top=0;
-			for(j=0; j<n_sites; j++){
-				dist=0;
-				for(d=0; d<DIM; d++) dist += floatpow( ind2j(d,i,L)-ind2j(d,j,L) ,2);
-				dist = sqrt(dist);
-				if(dist <= params.R && dist > (params.R - 1)){
-					crown[i][c_top]=j;
-					c_top++;
-				}
-			}
-		}
-		
-		// Evolve
-		evolveB(&phi, &y_colloid, neighbours, n_sites, n_timestep, &params, mosaic, top, crown, c_top);
-		
-		// Free crown list
-		for(i=0; i<n_sites; i++) free(crown[i]);
-		free(crown);
+		evolveB(&phi, &y_colloid, neighbours, n_sites, n_timestep, &params);
 	}
-		
 		
 	// Free memory and exit
 	free(phi);
 	free(y_colloid);
-	for(i=0; i<n_sites; i++){
-		free(neighbours[i]);
-		free(mosaic[i]);
-	}
-	free(mosaic);
+	for(i=0; i<n_sites; i++) free(neighbours[i]);
 	return 0;
 }
 
@@ -210,14 +161,13 @@ void default_parameters(parameter* params){
 	params->temperature = 1.0;
 	params->relativeD = 1.0;
 	params->trap_strength = 1.0;
-	params->rng_seed = -1; // if seed is -1 (not given by user), it will be picked randomly in 'initialise'
+	params->rng_seed = -1; 												// if seed is -1 (not given by user), it will be picked randomly in 'initialise'
 	params->system_size = 32;
 	params->delta_t = 0.0001;
-	params->R = 2;
 }
 
 // All that needs to be done once
-int initialise(double** phi, double** y_colloid, long*** neighbours, long n_sites, parameter* params, long*** mosaic){
+void initialise(double** phi, double** y_colloid, long*** neighbours, long n_sites, parameter* params){
 	// i) GSL random number generator setup
 	gsl_rng_env_setup();
         T = gsl_rng_default;
@@ -227,7 +177,7 @@ int initialise(double** phi, double** y_colloid, long*** neighbours, long n_site
         printf("# RNG Seed %i\n",params->rng_seed);
 	
 	// ii) Initialise physical variables: phi, y
-	long i, j;					// We sample from an infinite temperature state. Maybe something else would be better.
+	long i, j;															// We sample from an infinite temperature state. Maybe something else would be better.
 	for(i = 0; i < n_sites; i++){ 
 		 (*phi)[i] = gsl_ran_gaussian_ziggurat(r,1.0); 
 	}
@@ -238,24 +188,6 @@ int initialise(double** phi, double** y_colloid, long*** neighbours, long n_site
 	
 	// iii) What are each position's neighbours?
 	neighborhood(neighbours,L);
-	
-	// iv) Initialize the mosaic
-	double dist;
-	int top, d;
-	for(i=0; i<n_sites; i++){
-		top=0;
-		for(j=0; j<n_sites; j++){
-			dist=0;
-			for(d=0; d<DIM; d++) dist += floatpow( ind2j(d,i,L)-ind2j(d,j,L) ,2);
-			dist = sqrt(dist);
-			if(dist <= params->R){
-				(*mosaic)[i][top]=j;
-				top++;
-			}
-		}
-	}
-	
-	return top;															// Number of sites within R around a given site
 }
 
 // Creates list of nearest neighbours in DIM dimensions
@@ -279,7 +211,7 @@ void neighborhood(long*** list, int L){
 }
 
 // Model B evolution
-void evolveB(double** phi, double** y_colloid, long** neighbours, long n_sites, long n_timestep, parameter* params, long** mosaic, int top, long** crown, int c_top){
+void evolveB(double** phi, double** y_colloid, long** neighbours, long n_sites, long n_timestep, parameter* params){
 	// Preparing variables for field evolution (Euler-Maruyama)
 	long tstep;
 	double* laplacian_phi;
@@ -311,8 +243,7 @@ void evolveB(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 		
 		for(i=0; i<DIM; i++){ 														// Evolve each of the components separately
 			// Compute gradient of the field under the colloid
-			grad = 0;
-			for(j=0; j<top; j++) grad += 0.5 * ( (*phi)[mosaic[y_site][j]+intpow(L,i)] - (*phi)[mosaic[y_site][j]-intpow(L,i)] );
+			grad = 0.5 * ( (*phi)[y_site+intpow(L,i)] - (*phi)[y_site-intpow(L,i)] );
 			
 			// Compute temporary position
 			noise = noise_intensity * gsl_ran_gaussian_ziggurat(r, 1.0);
@@ -322,22 +253,21 @@ void evolveB(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 		
 		// iii) Evolve the field with the local copy of the colloid position	
 		// I'm sure this can be optimised, but at least its explicit enough to see what's going on
-		laplacian( &laplacian_phi, *phi, neighbours, n_sites);						// Write Laplacian of phi into laplacian_phi
-		laplacian( &laplacian_square_phi, laplacian_phi, neighbours, n_sites);		// Write (D^2)^2 phi into laplacian_square_phi 
+		laplacian(&laplacian_phi, *phi, neighbours, n_sites);						// Write Laplacian of phi into laplacian_phi
+		laplacian(&laplacian_square_phi, laplacian_phi, neighbours, n_sites);		// Write (D^2)^2 phi into laplacian_square_phi 
 		laplacian_of_cube(&laplacian_phi_cubed, *phi, neighbours, n_sites);			// Write D2 [phi(x)^3] into laplacian_phi_cubed
 		generate_noise_field(&noise_field, DIM*n_sites, params);					// Fill \vec{Lambda} with randomness
-		gradient_field(&noise_gradient, noise_field, neighbours,  n_sites);			// Compute gradient noise term
+		gradient_field(&noise_gradient, noise_field, neighbours, n_sites);			// Compute gradient noise term
 			
 		// Add together to new step d/dt phi = -a * D2 phi - b D4 phi - u D2 (phi^3) + D * noise (D is nabla)
-		phi_evolveB(phi, laplacian_phi, laplacian_square_phi, laplacian_phi_cubed, noise_gradient, n_sites, params, Y, crown, c_top);
+		phi_evolveB(phi, laplacian_phi, laplacian_square_phi, laplacian_phi_cubed, noise_gradient, n_sites, params, Y, neighbours);
 			
 		// iv) Correction step for the colloid (with the evolved field)
 		y_site = closest_site(*y_colloid,L);										// Compute new site of the colloid
 		
 		for(i=0; i< DIM; i++){														// Evolve each of the components separately
 			// Compute new gradient
-			grad = 0;
-			for(j=0; j<top; j++) grad += 0.5 * ( (*phi)[mosaic[y_site][j]+intpow(L,i)] - (*phi)[mosaic[y_site][j]-intpow(L,i)] );
+			grad = 0.5 * ( (*phi)[y_site+intpow(L,i)] - (*phi)[y_site-intpow(L,i)] );
 			
 			// Compute corrected contribution
 			F2 = params->relativeD * (params->lambda*grad -params->trap_strength*( (*y_colloid)[i]-w[i] ));
@@ -353,7 +283,7 @@ void evolveB(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 }
 
 // Model A evolution
-void evolveA(double** phi, double** y_colloid, long** neighbours, long n_sites, long n_timestep, parameter* params, long** mosaic, int top){
+void evolveA(double** phi, double** y_colloid, long** neighbours, long n_sites, long n_timestep, parameter* params){
 	// Preparing variables for field evolution (Euler-Maruyama)
 	long tstep;
 	double* laplacian_phi;
@@ -380,8 +310,7 @@ void evolveA(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 		
 		for(i=0; i<DIM; i++){ 														// Evolve each of the components separately
 			// Compute gradient of the field under the colloid
-			grad = 0;
-			for(j=0; j<top; j++) grad += 0.5 * ( (*phi)[mosaic[y_site][j]+intpow(L,i)] - (*phi)[mosaic[y_site][j]-intpow(L,i)] );
+			grad = 0.5 * ( (*phi)[y_site+intpow(L,i)] - (*phi)[y_site-intpow(L,i)] );
 			
 			// Compute temporary position
 			noise = noise_intensity * gsl_ran_gaussian_ziggurat(r, 1.0);
@@ -390,20 +319,19 @@ void evolveA(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 		}
 	
 		// iii) Evolve the field with the local copy of the colloid position	
-		laplacian( &laplacian_phi, *phi, neighbours, n_sites);					// Write Laplacian of phi into laplacian_phi
-		generate_noise_field(&noise_field, n_sites, params);					// Fill Lambda with randomness
+		laplacian(&laplacian_phi, *phi, neighbours, n_sites);						// Write Laplacian of phi into laplacian_phi
+		generate_noise_field(&noise_field, n_sites, params);						// Fill Lambda with randomness
 				
 		// Add together to new step d/dt phi = -r * phi + \nabla^2 phi - u * (phi^3) - l * V(x-Y) + noise
-		phi_evolveA(phi, laplacian_phi, noise_field, n_sites, params, Y, top, mosaic);
+		phi_evolveA(phi, laplacian_phi, noise_field, n_sites, params, Y);
 		
 		// iv) Correction step for the colloid (with the evolved field)
-		y_site = closest_site(*y_colloid,L);									// Compute new site of the colloid
+		y_site = closest_site(*y_colloid,L);										// Compute new site of the colloid
 		 	
-		for(i=0; i< DIM; i++){													// Evolve each of the components separately
+		for(i=0; i< DIM; i++){														// Evolve each of the components separately
 			// Compute new gradient
-			grad = 0;
-			for(j=0; j<top; j++) grad += 0.5 * ( (*phi)[mosaic[y_site][j]+intpow(L,i)] - (*phi)[mosaic[y_site][j]-intpow(L,i)] );
-			
+			grad = 0.5 * ( (*phi)[y_site+intpow(L,i)] - (*phi)[y_site-intpow(L,i)] );
+						
 			// Compute corrected contribution
 			F2 = params->relativeD * (params->lambda*grad -params->trap_strength*( (*y_colloid)[i]-w[i] ));
 			
@@ -413,8 +341,38 @@ void evolveA(double** phi, double** y_colloid, long** neighbours, long n_sites, 
 		}
 
 		// v) Measures
-		measure(phi, tstep, params); 											// Evaluate all sorts of correlators etc. here
+		measure(phi, tstep, params); 												// Evaluate all sorts of correlators etc. here
 	}
+}
+
+// Field evolution - Model B
+void phi_evolveB(double** phi, double* laplacian_phi, double* laplacian_square_phi, double* laplacian_phi_cubed, double* noise_gradient, long n_sites, parameter* params, double* Y, long ** neighbours){
+	long i;
+	long L = params->system_size;
+	double delta_t = params->delta_t;
+	for(i = 0; i < n_sites; i++){
+		(*phi)[i] += (delta_t*(-laplacian_square_phi[i] - params->mass*laplacian_phi[i] - params->quartic_u * laplacian_phi_cubed[i]) + noise_gradient[i]);
+	}
+	
+	// Interaction with the colloid
+	int y_site = closest_site(Y,L);													// Finds the lattice site closest to Y, the colloid
+	(*phi)[y_site] += delta_t * params->lambda *2*DIM;								// Ben, I think the normalization 1/2d should not have been there. Hope I was right D:
+	for(i = 0; i < 2*DIM; i++) (*phi)[neighbours[y_site][i]] -= delta_t * params->lambda;
+}
+
+// Field evolution - Model A
+void phi_evolveA(double** phi, double* laplacian_phi, double* noise_field, long n_sites, parameter* params, double* Y){
+	long i;
+	double delta_t = params->delta_t;
+	long L = params->system_size;
+	for(i = 0; i < n_sites; i++)													// Notice noise_field already contains delta_t in its variance
+	{
+		(*phi)[i] += delta_t*(- params->mass*(*phi)[i] + laplacian_phi[i] - params->quartic_u * floatpow((*phi)[i],3)  ) + noise_field[i];
+	}
+	
+	// Interaction with the colloid
+	int y_site = closest_site(Y,L);													// Finds the lattice site closest to Y, the colloid
+	(*phi)[y_site] += delta_t * params->lambda;										// Adds the interaction at that site
 }
 
 // Returns Laplacian as calculated from cubic neighbour cells in DIM dimensions
@@ -467,39 +425,6 @@ void gradient_field(double** grad_noise, double* noise, long** neighbours, long 
 	}
 }
 
-// Physics happens - Model B
-void phi_evolveB(double** phi, double* laplacian_phi, double* laplacian_square_phi, double* laplacian_phi_cubed, double* noise_gradient, long n_sites, parameter* params, double* Y, long** crown, int c_top){
-	long i;
-	long L = params->system_size;
-	double delta_t = params->delta_t;
-	for(i = 0; i < n_sites; i++){
-		(*phi)[i] += (delta_t*(-laplacian_square_phi[i] - params->mass*laplacian_phi[i] - params->quartic_u * laplacian_phi_cubed[i]) + noise_gradient[i]);
-	}
-	
-	// Interaction with the colloid
-	int y_site = closest_site(Y,L);										// Finds the lattice site closest to Y, the colloid
-	for(i=0; i<c_top; i++){
-		(*phi)[crown[y_site][i]] -= delta_t * params->lambda * (1-(DIM-1)/params->R);
-	}
-}
-
-// Physics happens - Model A
-void phi_evolveA(double** phi, double* laplacian_phi, double* noise_field, long n_sites, parameter* params, double* Y, int top, long** mosaic){
-	long i;
-	double delta_t = params->delta_t;
-	long L = params->system_size;
-	for(i = 0; i < n_sites; i++)										// Notice noise_field already contains delta_t in its variance
-	{
-		(*phi)[i] += delta_t*(- params->mass*(*phi)[i] + laplacian_phi[i] - params->quartic_u * floatpow((*phi)[i],3)  ) + noise_field[i];
-	}
-	
-	// Interaction with the colloid
-	int y_site = closest_site(Y,L);										// Finds the lattice site closest to Y, the colloid
-	for(i=0; i<top; i++){
-		(*phi)[mosaic[y_site][i]] -= delta_t * params->lambda;
-	}
-}
-
 // Perform measurements and print them out
 void measure(double** phi, long tstep, parameter* params){
 	long i;
@@ -523,7 +448,7 @@ int closest_site(double *vec, int L){
 	return site;
 }
 
-// Factorial of a number// Factorial of a number
+// Factorial of a number
 unsigned int factorial(unsigned int n){
     if (n == 0) return 1;
     return n * factorial(n - 1);
@@ -553,7 +478,6 @@ void printhelp(void){
 # -T Temperature of bath\n\
 # -D Relative motility colloid/field\n\
 # -k Strength of harmonic trap\n\
-# -R Colloid radius\n\
 # -S Seed for RNG\n\
 # -h To see this helpscreen\n\
 # -X Output source code\n",__FILE__,__DATE__);
